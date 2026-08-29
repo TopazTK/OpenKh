77e173686125be0a28de7d86d1d7c9dc59785a3f
TopazTK <250328010.b@uni-prizren.com>
2026-08-29 10:01:26 +0200

    [Mod Manager] - Libraries.

--- OpenKh.Tools.ModManager/App.axaml.cs
+++ OpenKh.Tools.ModManager/App.axaml.cs
@@ -1,8 +1,11 @@
-﻿using Avalonia;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using OpenKh.Tools.ModManager.Services;
 using OpenKh.Tools.ModManager.ViewModels;
 using OpenKh.Tools.ModManager.Views;
+using SharpYaml;
+using System.IO;
 
 namespace OpenKh.Tools.ModManager;
 
@@ -17,7 +20,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
+            desktop.MainWindow = new MainView
             {
                 DataContext = new MainViewModel(),
             };
