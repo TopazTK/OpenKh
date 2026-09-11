ed45c4561418860d581b0323856394adc4dc8103
TopazTK <250328010.b@uni-prizren.com>
2026-09-11 17:47:35 +0200

    [Launcher] - Pass proper arguments.

--- OpenKh.Command.Launcher/Program.cs
+++ OpenKh.Command.Launcher/Program.cs
@@ -9,14 +9,15 @@ namespace OpenKh.Command.Launcher
             {"kh1", "KINGDOM HEARTS FINAL MIX.exe" },
             {"kh2", "KINGDOM HEARTS II FINAL MIX.exe" },
             {"recom", "KINGDOM HEARTS Re_Chain of Memories.exe" },
-            {"bbs", "KINGDOM HEARTS Birth by Sleep FINAL MIX.exe" }
+            {"bbs", "KINGDOM HEARTS Birth by Sleep FINAL MIX.exe" },
+            {"ddd", "KINGDOM HEARTS Dream Drop Distance.exe" },
         };
 
         static string MAIN_LAUNCH = "KINGDOM HEARTS HD 1.5+2.5 Launcher.exe";
 
         public static void Main(string[] args)
         {
-            var _fetchArguments = KEY_LAUNCH.FirstOrDefault(x => x.Key == args[0]).Key == "" ? String.Join(" ", args.Skip(1)) : String.Join(" ", args);
+            var _fetchArguments = KEY_LAUNCH.FirstOrDefault(x => x.Key == args[0]).Key != "" ? String.Join(" ", args.Skip(1)) : String.Join(" ", args);
             var _launchPath = args.Length == 0 || !KEY_LAUNCH.ContainsKey(args[0]) ? Path.Combine(AppContext.BaseDirectory, MAIN_LAUNCH) : Path.Combine(AppContext.BaseDirectory, KEY_LAUNCH[args[0]]);
 
             Process.Start(_launchPath, _fetchArguments);
