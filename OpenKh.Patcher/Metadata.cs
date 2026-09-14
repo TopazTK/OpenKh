31eb6913ce80cd2001f9848afebc60e5f9a9afe9
TopazTK <250328010.b@uni-prizren.com>
2026-09-14 23:20:51 +0200

    [Mod Manager] - Fix duplication.

--- OpenKh.Patcher/Metadata.cs
+++ OpenKh.Patcher/Metadata.cs
@@ -34,63 +34,25 @@ namespace OpenKh.Patcher
         [YamlIgnore]
         public bool IsValid = true;
 
-        public static Metadata Read(string fileName)
+        public static Metadata? Read(object inputFormat)
         {
-            var _fetchYamlRaw = File.ReadAllText(fileName);
+            string? _fetchYamlRAW = null;
+
+            if (inputFormat is StringReader)
+                _fetchYamlRAW = (inputFormat as StringReader).ReadToEnd();
+
+            else if (inputFormat is string)
+                _fetchYamlRAW = File.ReadAllText(inputFormat as string);
+
+            else
+                return null;
 
             // Replace all back slashes as Windows supports forward slashes but Linux doesn't support back slashes.
-            _fetchYamlRaw = _fetchYamlRaw.Replace('\\', '/');
-
-            try 
-            { 
-                var _fetchSerial = YamlSerializer.Deserialize<Metadata>(_fetchYamlRaw, new YamlSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-
-                // A single mod should not be able to patch a file more than once.
-                // Ignore any and all duplicates that target the same file.
-                var _fetchAssetUnique = _fetchSerial.Assets.DistinctBy(x => new { x.Name, x.Platform });
-                _fetchSerial.Assets = _fetchAssetUnique.ToList();
-
-                return _fetchSerial;
-            }
-
-            catch (SharpYaml.YamlException ex)
-            {
-                // Handle YAML parsing errors
-                Debug.WriteLine($"Error deserializing YAML: {ex.Message}");
-
-                var _fetchTitle = string.Empty;
-                var _fetchMatch = Regex.Match(_fetchYamlRaw, @"(?<=title:).*", RegexOptions.None, TimeSpan.FromMilliseconds(500));
-
-                if (_fetchMatch.Success)
-                    _fetchTitle = _fetchMatch.Value.Trim();
-
-                var metadata = new Metadata
-                {
-                    Title = _fetchTitle,
-                    IsValid = false
-                };
-
-                return metadata; // Return modified metadata indicating failure
-            }
-
-            catch (Exception ex)
-            {
-                // Handle other unexpected errors
-                Debug.WriteLine($"Unexpected error: {ex.Message}");
-                throw; // Rethrow other exceptions for further investigation
-            }
-        }
-
-        public static Metadata Read(StringReader fileStream)
-        {
-            var _fetchYamlRaw = fileStream.ReadToEnd();
-
-            // Replace all back slashes as Windows supports forward slashes but Linux doesn't support back slashes.
-            _fetchYamlRaw = _fetchYamlRaw.Replace('\\', '/');
+            _fetchYamlRAW = _fetchYamlRAW.Replace('\\', '/');
 
             try
             {
-                var _fetchSerial = YamlSerializer.Deserialize<Metadata>(_fetchYamlRaw, new YamlSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+                var _fetchSerial = YamlSerializer.Deserialize<Metadata>(_fetchYamlRAW, new YamlSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
 
                 // A single mod should not be able to patch a file more than once.
                 // Ignore any and all duplicates that target the same file.
@@ -106,7 +68,7 @@ namespace OpenKh.Patcher
                 Debug.WriteLine($"Error deserializing YAML: {ex.Message}");
 
                 var _fetchTitle = string.Empty;
-                var _fetchMatch = Regex.Match(_fetchYamlRaw, @"(?<=title:).*", RegexOptions.None, TimeSpan.FromMilliseconds(500));
+                var _fetchMatch = Regex.Match(_fetchYamlRAW, @"(?<=title:).*", RegexOptions.None, TimeSpan.FromMilliseconds(500));
 
                 if (_fetchMatch.Success)
                     _fetchTitle = _fetchMatch.Value.Trim();
